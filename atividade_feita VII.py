e1522a010c1f55abc707d107a6490c95754a7f0a
aillan9 <aillanlima14@gmail.com>
2024-10-08 19:13:31 +0000

    atividade concluida com sucesso hehe

--- atividade_feita VII.py
+++ atividade_feita VII.py
@@ -1,31 +1,35 @@
+import os
+os.system("cls || clear")
 
 # Entrada
 nome = input("Informe seu nome completo: ")
-peso = float(input("Informe seu peso: "))
-altura = float(input("Informe sua altura: "))
+peso = float(input("Informe seu peso (em kg): "))
+altura = float(input("Informe sua altura (em metros): "))
 
-# Definindo funções
+# Processamento
 def calcular_imc(peso, altura):
-    resultado = peso / (altura * altura)
+    return peso / (altura ** 2)
 
 def classificar_imc(imc):
-   if imc <18.5:
-     return"Você está abaixo do peso!"
-   elif imc >= 18.5 and imc <= 24.9:
-      return"Você está no peso normal"
-   elif imc >= 25 and imc <= 29.9:
-      return"Sobrepeso"
-   elif imc >= 30 and imc <= 34.9:
-      return"Você está obeso no grau 1"
-   elif imc >= 35 and imc <= 39.9:
-      return"Você está obeso no grau 2"
-   elif imc >= 40:
-      return"Você está obeso no grau 3"
+    if imc < 18.5:
+        print("Você está abaixo do peso!")
+    elif 18.5 <= imc <= 24.9:
+        print("Você está no peso normal.")
+    elif 25 <= imc <= 29.9:
+        print("Você está com sobrepeso.")
+    elif 30 <= imc <= 34.9:
+        print("Você está obeso no grau 1.")
+    elif 35 <= imc <= 39.9:
+        print("Você está obeso no grau 2.")
+    elif imc >= 40:
+        print("Você está obeso no grau 3 (obesidade mórbida).")
+
+# Definindo imc
+imc = calcular_imc(peso, altura)
 
 # Saída 
-print(f"Nome completo:  {nome}")
-print(f"Altura:  {altura}")
-print(f"Peso:  {peso}")
-
+print(f"\nNome completo: {nome}")
+print(f"Altura: {altura} m")
+print(f"Peso: {peso} kg")
+print(f"IMC: {imc:.2f}")
 classificar_imc(imc)
-calcular_imc(peso, altura)
